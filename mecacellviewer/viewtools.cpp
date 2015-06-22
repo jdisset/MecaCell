1a496c90715affb33eab617056f7d2aa29dbec00
jean <j.disset@gmail.com>
2015-06-22 20:05:54 +0200

    glsl auto version

--- mecacellviewer/viewtools.cpp
+++ mecacellviewer/viewtools.cpp
@@ -6,8 +6,29 @@
 #include "viewtools.h"
 QOpenGLFunctions *GL = nullptr;
 QString shaderWithHeader(QString filename) {
-	QString version = QString::number(QOpenGLContext::currentContext()->format().majorVersion()) +
-	                  QString::number(QOpenGLContext::currentContext()->format().minorVersion()) + "0";
+	int majorV = QOpenGLContext::currentContext()->format().majorVersion();
+	int minorV = QOpenGLContext::currentContext()->format().minorVersion();
+	QString version;
+	if ((majorV == 3 && minorV >= 3) || majorV > 3) {
+		version = QString::number(majorV) + QString::number(minorV) + "0";
+	} else if (majorV == 3) {
+		switch (minorV) {
+			case 2:
+				version = "150";
+				break;
+			case 1:
+				version = "140";
+				break;
+			default:
+				version = "130";
+				break;
+		}
+	} else if (minorV == 1) {
+		version = "120";
+	} else {
+		version = "110";
+	}
+
 	QFile f(filename);
 	if (!f.open(QIODevice::ReadOnly | QIODevice::Text)) qDebug() << "unable to open " << filename << endl;
 	QTextStream in(&f);
