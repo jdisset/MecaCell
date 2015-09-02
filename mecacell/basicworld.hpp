28fde6a79f09c24b2b4f860d8af2945e82f166fd
jean <j.disset@gmail.com>
2015-09-02 16:48:41 +0200

    gcc compilation bug

--- mecacell/basicworld.hpp
+++ mecacell/basicworld.hpp
@@ -93,7 +93,7 @@ public:
 					grid.insert(c);
 				updateConnectionsLengthAndDirection();
 				cellCollisions();
-				// deleteImpossibleConnections();
+				 deleteImpossibleConnections();
 			}
 			updateBehavior();
 			destroyCells();
