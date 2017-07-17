490aa815b8bd16e43a483121e972d1cc77f2b8b5
Jean Disset <j.disset@gmail.com>
2017-07-17 22:04:26 +0200

    header only logger

--- mecacell/utilities/logger.cpp
+++ mecacell/utilities/logger.cpp
@@ -1,13 +1,3 @@
 #include "logger.hpp"
 namespace MecaCell {
-constexpr decltype(WARN::tag) WARN::tag;
-constexpr decltype(ERR::tag) ERR::tag;
-constexpr decltype(INF::tag) INF::tag;
-constexpr decltype(DBG::tag) DBG::tag;
-constexpr decltype(SUC::tag) SUC::tag;
-constexpr decltype(WARN::color) WARN::color;
-constexpr decltype(ERR::color) ERR::color;
-constexpr decltype(INF::color) INF::color;
-constexpr decltype(DBG::color) DBG::color;
-constexpr decltype(SUC::color) SUC::color;
 }
--- mecacell/utilities/logger.hpp
+++ mecacell/utilities/logger.hpp
@@ -56,6 +56,18 @@ const constexpr static char BOLDCYAN[] = "";
 const constexpr static char BOLDWHITE[] = "";
 #endif
 
+// maybe needed 
+//constexpr decltype(WARN::tag) WARN::tag;
+//constexpr decltype(ERR::tag) ERR::tag;
+//constexpr decltype(INF::tag) INF::tag;
+//constexpr decltype(DBG::tag) DBG::tag;
+//constexpr decltype(SUC::tag) SUC::tag;
+//constexpr decltype(WARN::color) WARN::color;
+//constexpr decltype(ERR::color) ERR::color;
+//constexpr decltype(INF::color) INF::color;
+//constexpr decltype(DBG::color) DBG::color;
+//constexpr decltype(SUC::color) SUC::color;
+
 template <typename T> std::string sublogger(T&& t) {
 	std::ostringstream os;
 	os << t;
